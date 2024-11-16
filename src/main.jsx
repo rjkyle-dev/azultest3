1828df50cd0d6850ed1f40b509b07691831dff7f
RJ Kyle G Labrador <kyledev10282001@gmail.com>
2024-11-16 14:20:40 +0800

    url again

--- src/main.jsx
+++ src/main.jsx
@@ -7,9 +7,9 @@ import App from './App.jsx'
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/azulcorp.com">
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="*" element={<App />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
