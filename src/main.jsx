efced671f45e06037e99fb42aa6595cafb437b7f
RJ Kyle G Labrador <kyledev10282001@gmail.com>
2024-11-16 14:28:10 +0800

    url

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
+import React from "react";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import './index.css'
-import App from './App.jsx'
+import "./index.css";
+import App from "./App.jsx";
 
+// Rendering the application
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter basename={import.meta.env.BASE_URL}>
+      {" "}
+      {/* Use the base path if needed */}
       <Routes>
-        <Route path="*" element={<App />} />
+        <Route path="*" element={<App />} /> {/* Catch-all route */}
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
