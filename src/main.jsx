2a8782f41c5cee0fc5bf3f3f0bf64f3911631546
RJ Kyle G Labrador <kyledev10282001@gmail.com>
2024-11-16 14:35:42 +0800

    Add GitHub Pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,15 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
+import React from 'react'
+import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import "./index.css";
-import App from "./App.jsx";
+import './index.css'
+import App from './App.jsx'
 
-// Rendering the application
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
-      {" "}
-      {/* Use the base path if needed */}
+    <BrowserRouter basename="/azultest3">
       <Routes>
-        <Route path="*" element={<App />} /> {/* Catch-all route */}
+        <Route path="/" element={<App />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
